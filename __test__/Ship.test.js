0e4ba5353b14042fb208161a01ad7e67b87f9cc3
Wasiu-hub <contactbow@yahoo.com>
2023-07-30 05:24:23 +0100

    Revert "#12c test for ship being added on instantiation"
    This reverts commit cbc5c35bc35347719ba8a995cc2192d36717c47c.

--- __test__/Ship.test.js
+++ __test__/Ship.test.js
@@ -25,14 +25,6 @@ describe("Ship", () => {
     // expect(ship.currentPort).toBe("Dover");
     expect(ship.currentPort).toBe(port);
   });
-
-  it("gets added to port on instantiation", () => {
-    const dover = new Port("Dover");
-    const itinerary = new Itinerary([dover]);
-    const ship = new Ship(itinerary);
-
-    expect(dover.ships).toContain(ship);
-  });
 });
 
 describe("setSail", () => {
